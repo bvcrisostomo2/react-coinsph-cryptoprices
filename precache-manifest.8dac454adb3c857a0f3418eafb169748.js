d7050c4e5d57e2dfb6ae8d40be1e213ee5a8d28b
bvcrisostomo2 <bvcrisostomo2@gmail.com>
2019-09-22 01:41:14 +0800

    Updates

---
+++ precache-manifest.8dac454adb3c857a0f3418eafb169748.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fc7cee7a8694c92b9ff34b8fa269996",
+    "url": "/react-coinsph-cryptoprices/index.html"
+  },
+  {
+    "revision": "467bccdadf0e82dadd03",
+    "url": "/react-coinsph-cryptoprices/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "08186266e9379799208d",
+    "url": "/react-coinsph-cryptoprices/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "467bccdadf0e82dadd03",
+    "url": "/react-coinsph-cryptoprices/static/js/2.27e336cb.chunk.js"
+  },
+  {
+    "revision": "08186266e9379799208d",
+    "url": "/react-coinsph-cryptoprices/static/js/main.480d6c28.chunk.js"
+  },
+  {
+    "revision": "86d11d16a7e75910bb0b",
+    "url": "/react-coinsph-cryptoprices/static/js/runtime~main.a3bb1a01.js"
+  }
+]);
