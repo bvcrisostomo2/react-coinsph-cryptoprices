9766c2853668fb3629b471af25e02dc1f7fa216d
bvcrisostomo2 <bvcrisostomo2@gmail.com>
2019-09-26 05:50:40 +0800

    added footer

---
+++ src/components/navbarMainFooter.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import { Layout, Header, Navigation, Content } from "react-mdl";
+
+import { MainContent } from "./mainContent";
+import { Link } from "react-router-dom";
+import { FooterComponent } from "./footerComponent";
+
+export class NavbarMainFooter extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <Layout fixedHeader>
+          <Header
+            title={
+              <span>
+                <span style={{ color: "#ddd" }}>Coins.ph</span>
+                <strong> Prices</strong>
+              </span>
+            }
+          >
+            <Navigation>
+              <Link to="/">Prices</Link>
+              <Link to="/donate">Donate</Link>
+            </Navigation>
+          </Header>
+          <Content>
+            <MainContent />
+            <FooterComponent />
+          </Content>
+        </Layout>
+      </div>
+    );
+  }
+}
+
+export default NavbarMainFooter;
