ce9e8ac3f156abca34285781337a157033062f4c
bvcrisostomo2 <bvcrisostomo2@gmail.com>
2019-09-25 21:19:37 +0800

    added qr code and grid

--- src/components/donatePage.js
+++ src/components/donatePage.js
@@ -1,11 +1,26 @@
 import React, { Component } from "react";
+import { Grid, Cell } from "react-mdl";
 
 export class DonatePage extends Component {
   state = {};
   render() {
     return (
       <div>
-        <h3>Bitcoin Address</h3>3QVw6mC9M556bnZPaQfv7oC8ox4z7hTTUk
+        <div style={{ width: "80%", margin: "auto" }}>
+          <Grid>
+            <Cell col={6}>
+              <div style={{ margin: "auto" }}>
+                <h3>Bitcoin Address</h3>
+                <img
+                  src={require("../images/btc-address.png")}
+                  alt="QR CODE"
+                  className="qr-img"
+                />
+                <div>3QVw6mC9M556bnZPaQfv7oC8ox4z7hTTUk</div>
+              </div>
+            </Cell>
+          </Grid>
+        </div>
       </div>
     );
   }
