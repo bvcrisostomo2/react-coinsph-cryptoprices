1c963976c29b1eb4df4046a5f27571876739315b
bvcrisostomo2 <bvcrisostomo2@gmail.com>
2019-09-25 21:22:39 +0800

    renamed marketpricespage

--- src/components/mainContent.js
+++ src/components/mainContent.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 //Route Components
-import { MarketPrices } from "./marketPrices";
+import { MarketPricesPage } from "./marketPricesPage";
 import { DonatePage } from "./donatePage";
 
 export class MainContent extends React.Component {
   render() {
     return (
       <Switch>
-        <Route exact path="/" component={MarketPrices} />
+        <Route exact path="/" component={MarketPricesPage} />
         <Route exact path="/donate" component={DonatePage} />
       </Switch>
     );
