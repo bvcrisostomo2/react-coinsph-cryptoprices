7c9dd58ec33bc9a3296bc229eb13230560f24d44
bvcrisostomo2 <bvcrisostomo2@gmail.com>
2019-10-08 21:38:53 +0800

    added landing page route

--- src/components/mainContent.js
+++ src/components/mainContent.js
@@ -3,6 +3,7 @@ import { Route, Switch } from "react-router-dom";
 //Route Components
 import { MarketPricesPage } from "./marketPricesPage";
 import { DonatePage } from "./donatePage";
+import LandingPage from "./landingPage";
 
 export class MainContent extends React.Component {
   render() {
@@ -10,6 +11,7 @@ export class MainContent extends React.Component {
       <Switch>
         <Route exact path="/" component={MarketPricesPage} />
         <Route exact path="/donate" component={DonatePage} />
+        <Route exact path="/landing" component={LandingPage} />
       </Switch>
     );
   }
