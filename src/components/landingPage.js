4d57ba0a0ece226f8a1ebe166c76c31a04c69c3c
bvcrisostomo2 <bvcrisostomo2@gmail.com>
2019-11-12 01:02:42 +0800

    polished landing page

--- src/components/landingPage.js
+++ src/components/landingPage.js
@@ -18,12 +18,16 @@ export class LandingPage extends Component {
                     width: "80%"
                   }}
                 >
+                  <img
+                    src={require("../images/value.png")}
+                    alt="value"
+                    className="landing-image"
+                  />
                   <h3 className="header-landing-1">
                     Track market prices anytime!
                   </h3>
                   <h5 className="header-landing-1">
-                    A convenient way to track market prices from Coins.ph! All
-                    data that comes from coins.ph is being tracked real time.
+                    A convenient way to track market prices from Coins.ph!
                   </h5>
                   <Link to="/">
                     <button className="button-landing">Check Prices!</button>
@@ -46,47 +50,45 @@ export class LandingPage extends Component {
               <Grid>
                 <Cell col={4} tablet={8} className="why-section-2">
                   <img
-                    src={require("../images/bitcoin-flat.png")}
-                    alt="bitcoin"
+                    src={require("../images/real-time-flat.png")}
+                    alt="api"
                     className="why-image"
                   />
-                  <p>
-                    Lorem Ipsum is simply dummy text of the printing and
-                    typesetting industry. Lorem Ipsum has been the industry's
-                    standard dummy text ever since the 1500s, when an unknown
-                    printer took a galley of type and scrambled it to make a
-                    type specimen book.
-                  </p>
+                  <h4>Real Time Prices</h4>
+                  <p>Prices are being tracked real time</p>
                 </Cell>
                 <Cell col={4} tablet={8} className="why-section-2">
                   <img
-                    src={require("../images/bitcoin-flat.png")}
-                    alt="bitcoin"
+                    src={require("../images/api-flat.png")}
+                    alt="real-time"
                     className="why-image"
                   />
-                  <p>
-                    Lorem Ipsum is simply dummy text of the printing and
-                    typesetting industry. Lorem Ipsum has been the industry's
-                    standard dummy text ever since the 1500s, when an unknown
-                    printer took a galley of type and scrambled it to make a
-                    type specimen book.
-                  </p>
+                  <h4>Legitimate Data</h4>
+                  <p>Data comes from coins.ph API</p>
                 </Cell>
                 <Cell col={4} tablet={8} className="why-section-2">
                   <img
-                    src={require("../images/bitcoin-flat.png")}
-                    alt="bitcoin"
+                    src={require("../images/exchange-flat.png")}
+                    alt="exchange"
                     className="why-image"
                   />
-                  <p>
-                    Lorem Ipsum is simply dummy text of the printing and
-                    typesetting industry. Lorem Ipsum has been the industry's
-                    standard dummy text ever since the 1500s, when an unknown
-                    printer took a galley of type and scrambled it to make a
-                    type specimen book.
-                  </p>
+                  <h4>Complete Fiat Pairs</h4>
+                  <p>Currently have BTC, ETH, XRP, and BCH to PHP</p>
                 </Cell>
               </Grid>
+            </div>
+          </div>
+          <div className="landing-section-three">
+            <div className="donate-section-three">
+              <h4>
+                Would you like to offer a sip?{" "}
+                <span role="img" aria-label="coffee">
+                  ☕
+                </span>
+              </h4>
+              <Link to="donate" className="links">
+                Go to Donate Page
+              </Link>
             </div>
           </div>
         </div>
